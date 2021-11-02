9d8566864b7eb8114b7d137bb0385986f6f85386
lottestu <lottestu@student.matnat.uio.no>
2021-11-02 16:02:09 +0100

    hamburger display js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,45 @@
-const menu = document.querySelector('.frontpage__hamburger');
-menu.addEventListener('click', displayMenu);
 
-function displayMenu() {
-    console.log('')
-    /*
-        1. if menu === none
-            display
-        2. else {
-            none
-        }
-    */
+
+function displayHamburgerMenu() {
+    const hamburgerMenu = document.querySelector('.frontpage__hamburgermenu');
+    const exit = document.querySelector('.frontpage__hamburgerExit');
+
+    hamburgerMenu.style.display = 'block'; 
+
+    exit.addEventListener('click', event => {
+        hamburgerMenu.style.display = 'none';
+    }) 
 }
+
+
+
+
+/********** event listeners ***************/
+
+/* hamburger button */
+const hamburger = document.querySelector('.frontpage__hamburger');
+hamburger.addEventListener('click', displayHamburgerMenu);
+
+/* ham navigation */
+const hamburgerNavButtons = document.querySelectorAll('.frontpage__hamburgernav button');
+[...hamburgerNavButtons].forEach(button => {
+    button.addEventListener('click', event => {
+        if (button.innerText === 'Home'){
+            window.location.href = 'index.html'
+        }
+
+        if (button.innerText === 'About'){
+            window.location.href = 'about.html'
+        }
+
+        if (button.innerText === 'Gallery') {
+            window.location.href = 'gallery.html';
+        }
+
+        if (button.innerText === 'Contact'){
+            window.location.href = 'contact.html'
+        }
+    })
+})
+
+
