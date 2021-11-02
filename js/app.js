d06bdb11a395d02f073c9239970e6b5fb1c916c9
lottestu <lottestu@student.matnat.uio.no>
2021-11-02 13:45:54 +0100

    frontpage responsive

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector('.frontpage__hamburger');
+menu.addEventListener('click', displayMenu);
+
+function displayMenu() {
+    console.log('')
+    /*
+        1. if menu === none
+            display
+        2. else {
+            none
+        }
+    */
+}
