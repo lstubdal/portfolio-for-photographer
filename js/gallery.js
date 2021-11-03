f79eb3586d4bc9cf032488d429ad8bc406cc3c9d
lottestu <lottestu@student.matnat.uio.no>
2021-11-03 16:49:45 +0100

    responsive gallery srcset

--- js/gallery.js
+++ js/gallery.js
@@ -4,24 +4,23 @@ const gallery =  {
         {text: 'See the world through patterns'},
     ],
     images: [
-        {title: 'Skyscraper', year: 1997, location: 'Kairo', file: 'assets/img/gallery/skyscraper.jpg'},
-        {title: 'Copperwall', year: 2010, location: 'Dubai', file: 'assets/img/gallery/copper-wall.jpg'},
-        {title: 'White building', year: 1998, location: 'Copenhagen', file: 'assets/img/gallery/white-building.jpg'},
-        {title: 'Wooden wall', year: 2018, location: 'Riga', file: 'assets/img/gallery/wooden-wall.jpg'},
+        {title: 'Skyscraper', year: 1997, location: 'Kairo', file: './assets/img/gallery/skyscraper.jpg', mobile: './assets/img/gallery/mobile/skyscraper.jpg', tablet: './assets/img/gallery/tablet/skyscraper.jpg'},
+        {title: 'Copperwall', year: 2010, location: 'Dubai', file: './assets/img/gallery/copper-wall.jpg', mobile: './assets/img/gallery/mobile/copper-wall.jpg', tablet: './assets/img/gallery/tablet/copper-wall.jpg'},
+        {title: 'White building', year: 1998, location: 'Copenhagen', file: './assets/img/gallery/white-building.jpg', mobile: './assets/img/gallery/mobile/white-building.jpg', tablet: './assets/img/gallery/tablet/white-building.jpg'},
+        {title: 'Wooden wall', year: 2018, location: 'Riga', file: './assets/img/gallery/wooden-wall.jpg', mobile: './assets/img/gallery/mobile/wooden-wall.jpg', tablet: './assets/img/gallery/tablet/wooden-wall.jpg'},
 
         {title: 'quote1', year: 0, location: '', file: ''},    
 
-        {title: 'Skyscraper', year: 1997, location: 'Kairo', file: 'assets/img/gallery/skyscraper.jpg'},
-        {title: 'Copperwall', year: 2010, location: 'Dubai', file: 'assets/img/gallery/copper-wall.jpg'},
-        {title: 'White building', year: 1998, location: 'Copenhagen', file: 'assets/img/gallery/white-building.jpg'},
-        {title: 'Wooden wall', year: 2018, location: 'Riga', file: 'assets/img/gallery/wooden-wall.jpg'},
-
+        {title: 'Skyscraper', year: 1997, location: 'Kairo', file: 'assets/img/gallery/skyscraper.jpg', mobile: 'assets/img/gallery/mobile/skyscraper.jpg', tablet: 'assets/img/gallery/tablet/skyscraper.jpg'},
+        {title: 'Copperwall', year: 2010, location: 'Dubai', file: 'assets/img/gallery/copper-wall.jpg', mobile: 'assets/img/gallery/mobile/copper-wall.jpg', tablet: 'assets/img/gallery/tablet/copper-wall.jpg'},
+        {title: 'White building', year: 1998, location: 'Copenhagen', file: 'assets/img/gallery/white-building.jpg', mobile: 'assets/img/gallery/mobile/white-building.jpg', tablet: 'assets/img/gallery/tablet/white-building.jpg'},
+        {title: 'Wooden wall', year: 2018, location: 'Riga', file: 'assets/img/gallery/wooden-wall.jpg', mobile: 'assets/img/gallery/mobile/wooden-wall.jpg', tablet: 'assets/img/gallery/tablet/wooden-wall.jpg'},
         {title: 'quote2', year: 0, location: '', file: ''},
 
-        {title: 'Skyscraper', year: 1997, location: 'Kairo', file: 'assets/img/gallery/skyscraper.jpg'},
-        {title: 'Copperwall', year: 2010, location: 'Dubai', file: 'assets/img/gallery/copper-wall.jpg'},
-        {title: 'White building', year: 1998, location: 'Copenhagen', file: 'assets/img/gallery/white-building.jpg'},
-        {title: 'Wooden wall', year: 2018, location: 'Riga', file: 'assets/img/gallery/wooden-wall.jpg'},
+        {title: 'Skyscraper', year: 1997, location: 'Kairo', file: 'assets/img/gallery/skyscraper.jpg', mobile: 'assets/img/gallery/mobile/skyscraper.jpg', tablet: 'assets/img/gallery/tablet/skyscraper.jpg'},
+        {title: 'Copperwall', year: 2010, location: 'Dubai', file: 'assets/img/gallery/copper-wall.jpg', mobile: 'assets/img/gallery/mobile/copper-wall.jpg', tablet: 'assets/img/gallery/tablet/copper-wall.jpg'},
+        {title: 'White building', year: 1998, location: 'Copenhagen', file: 'assets/img/gallery/white-building.jpg', mobile: 'assets/img/gallery/mobile/white-building.jpg', tablet: 'assets/img/gallery/tablet/white-building.jpg'},
+        {title: 'Wooden wall', year: 2018, location: 'Riga', file: 'assets/img/gallery/wooden-wall.jpg', mobile: 'assets/img/gallery/mobile/wooden-wall.jpg', tablet: 'assets/img/gallery/tablet/wooden-wall.jpg'},
     ]
 }
 
@@ -38,7 +37,6 @@ function setUpGallery() {
             imageContainer.className = '.galleryContainer__image galleryContainer__image--big';
         }
 
-        
         if (index === 4) {
             const quoteContainer = document.createElement('div');
             quoteContainer.className = 'galleryContainer__quote--left';
@@ -62,12 +60,25 @@ function setUpGallery() {
         } else { 
             
             const imageElement = document.createElement('img');
+            imageElement.srcset = `${gallery.images[index].mobile} 440w,
+                                    ${gallery.images[index].tablet} 640w,
+                                    ${gallery.images[index].file} 800w`;
+            imageElement.sizes = `(max-width: 420px) 440px, 
+                                    (max-width: 660px) 640px,
+                                    800px`;
             imageElement.src = gallery.images[index].file;
-
             imageContainer.appendChild(imageElement);
             galleryContainer.appendChild(imageContainer);
         }
     }
 }
+function displayHamMenu(){
+    console.log('button clicked');
 
+}
 setUpGallery();
+
+/****** Event listeners *********/
+
+const logo = document.querySelector('.gallery__nav--mobile');
+logo.addEventListener('click', displayHamMenu);
